c33833341368df021be0cb2d746ea543be2da023
James Ramsay <james@jramsay.com.au>
2019-02-23 08:55:37 -0500

    Update Snazzy.js

--- themes/Snazzy.js
+++ themes/Snazzy.js
@@ -1,4 +1,4 @@
 t.prefs_.set('color-palette-overrides',["#282a36", "#34353e", "#43454f", "#78787e", "#a5a5a9", "#e2e4e5", "#eff0eb", "#f1f1f0", "#ff5c57", "#ff9f43", "#f3f99d", "#5af78e", "#9aedfe", "#57c7ff", "#ff6ac1", "#b2643c"]);
-t.prefs_.set('foreground-color', "#ff6ac1");
+t.prefs_.set('foreground-color', "#eff0eb");
 t.prefs_.set('background-color', "#282a36");
-t.prefs_.set('cursor-color', 'rgba(16,16,21,0.5)');
+t.prefs_.set('cursor-color', '#97979b');
